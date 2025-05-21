bcf8772d1baa0df0ef8db4d230bb5f6186534f60
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2025-05-21 09:16:45 -0400

    CSV

--- MVAPlugin.R
+++ MVAPlugin.R
@@ -10,6 +10,8 @@ run <- function() {}
 
 output <- function(outputfile) {
 	pdf(outputfile)
+	write.csv(d_normalized@assays@data, paste(outputfile, "csv", sep="."))
+	#print(str(d_normalized))
         mvaresults = mva(d_normalized, measure="Area", method="PCA")
 plot_mva(mvaresults, color_by="group", components = c(1,2))
 
